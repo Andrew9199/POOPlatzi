23d086a0cc0f7ecfe821c9548aad5cfbe4f97612
Andrew Williams <alexissanchezd97@gmail.com>
2023-03-02 23:30:27 -0500

    30 implementacion de clases en Java y JavaScript y parcialmente en PHP

---
+++ JS/Card.js
@@ -0,0 +1,9 @@
+class Card extends Payment{
+    
+    constructor(number, CVV, date){
+        super();
+        this.number;
+        this.CVV;
+        this.date;
+    }
+}
---
+++ PHP/driver.php
@@ -0,0 +1,10 @@
+<?php
+    require_once('account.php');
+    class driver extends account{
+
+
+    public function __construct($name, $document){
+        parent::__construct($name, $document);
+    }
+    }
+?>
